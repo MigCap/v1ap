908ceb932a854e07f1886789e006c426444b8248
Miguel Capellán <miguelcapellan@gmail.com>
2024-12-20 20:20:39 +0100

    Clear unused config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -51,12 +51,6 @@ export const config = {
   greenColor: "#64ffda",
   navyColor: "#0a192f",
 
-  // hamburgerWidth: 30,
-  hamBefore: `top 0.1s ease-in 0.25s, opacity 0.1s ease-in`,
-  hamBeforeActive: `top 0.1s ease-out, opacity 0.1s ease-out 0.12s`,
-  hamAfter: `bottom 0.1s ease-in 0.25s, transform 0.22s cubic-bezier(0.55, 0.055, 0.675, 0.19)`,
-  hamAfterActive: `bottom 0.1s ease-out, transform 0.22s cubic-bezier(0.215, 0.61, 0.355, 1) 0.12s`,
-
   srConfig: (delay = 200) => {
     return {
       origin: "bottom",
