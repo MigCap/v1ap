3429a29dd7a4d8db6a242eadc20ae8b3443c6a08
Miguel Capellán <miguelcapellan@gmail.com>
2024-12-20 14:16:20 +0100

    Add additional sections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,15 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
-
-// 2. Import loader(s)
 import { glob } from "astro/loaders";
 
-// 3. Define your collection(s)
+const about = defineCollection({
+  loader: glob({ base: "./src/content/about", pattern: "**/*.md" }),
+  schema: z.object({
+    title: z.string(),
+    skills: z.array(z.string()),
+    avatar: z.string().url(),
+  }),
+});
+
 const jobs = defineCollection({
   loader: glob({ base: "./src/content/jobs", pattern: "**/*.md" }),
   schema: z.object({
@@ -16,5 +21,37 @@ const jobs = defineCollection({
   }),
 });
 
-// 4. Export a single `collections` object to register your collection(s)
-export const collections = { jobs };
+const featured = defineCollection({
+  // loader: glob({ base: "./src/content/featured", pattern: "**/*.md" }),
+  schema: ({ image }) =>
+    z.object({
+      date: z.coerce.date(),
+      title: z.string(),
+      cover: image(),
+      github: z.string().url().optional(),
+      external: z.string().url().optional(),
+      tech: z.array(z.string()),
+    }),
+});
+
+const projects = defineCollection({
+  loader: glob({ base: "./src/content/projects", pattern: "**/*.md" }),
+  schema: z.object({
+    date: z.coerce.date(),
+    title: z.string(),
+    image: z.string().optional(),
+    github: z.string().optional(),
+    external: z.string().optional(),
+    tech: z.array(z.string()),
+    show: z.string(),
+  }),
+});
+
+const contact = defineCollection({
+  loader: glob({ base: "./src/content/contact", pattern: "**/*.md" }),
+  schema: z.object({
+    title: z.string(),
+  }),
+});
+
+export const collections = { about, jobs, featured, projects, contact };
